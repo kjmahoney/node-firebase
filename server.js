f34624a2b20f8df0478b9d6194964d6fd24b9b87
Kevin Mahoney <kjmahoney13@gmail.com>
2017-11-26 23:59:30 -0500

    set up basic post

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('views'));
+app.set('views', __dirname + '/views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(logger('dev'));
 
 app.get('/', (request, response) => {
-  response.send('<h1>Cheeseburger</h1>');
+  response.render('home.ejs');
+})
+
+app.post('/', (request, response) => {
+  let chinese = request.body.chinese;
+
+  response.render('results.ejs', {data: chinese})
 })
 
 app.listen(port, () => {
